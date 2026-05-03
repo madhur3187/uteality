3040461c5469c64e720ef1a5722a928b7567880b
madhur3187 <malikmadhur31@gmail.com>
2026-05-03 16:41:27 +0530

    Updated dashboard design

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
 window.db = window.supabase.createClient(
   "https://rryafnqhfrrjglfxlwqw.supabase.co",
-  "YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyeWFmbnFoZnJyamdsZnhsd3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MzczNjcsImV4cCI6MjA5MzMxMzM2N30._eyInLCofs4HjMCvtgYGT_r6-Ujjhyq1AUyGb1PRzzc"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyeWFmbnFoZnJyamdsZnhsd3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MzczNjcsImV4cCI6MjA5MzMxMzM2N30._eyInLCofs4HjMCvtgYGT_r6-Ujjhyq1AUyGb1PRzzc"
 )
